40fe513e657955b8b941991b77b7aa1bbc7a7183
Yurii <juretz32@yandex.ru>
2024-07-16 20:21:35 +0300

    Better vpn-connect
    When VPN is on, just run RDP without reconnection

--- vpn-rdp.el
+++ vpn-rdp.el
@@ -6,7 +6,8 @@
 (define-derived-mode vpn-mode nil "vpn-helper"
   (setq-local vpn-buffer vpn-buffer)
   (setq-local vpn-server vpn-server)
-  (setq-local vpn-password vpn-password))
+  (setq-local vpn-password vpn-password)
+  (setq-local connected nil))
 
 
 (progn
@@ -48,45 +49,55 @@
           password))
 
 
-(defmacro define-vpn-command (name command)
-  `(defun ,(intern (concat "vpn-" (symbol-name name))) ()
-     (interactive)
-     (erase-buffer)
-     (make-process :name ,(concat "vpn-" (symbol-name name))
-                   :buffer vpn-buffer
-                   :command ,command
-                   :coding '(utf-8-unix . utf-8-unix)
-                   :filter (lambda (proc string)
-                             (when (buffer-live-p (process-buffer proc))
-                               (with-current-buffer (process-buffer proc)
-                                 (let ((moving (= (point) (process-mark proc))))
-                                   (save-excursion
-                                     (goto-char (process-mark proc))
-                                     (insert (replace-regexp-in-string "" "" string))
-                                     (set-marker (process-mark proc) (point)))
-                                   (if moving (goto-char (process-mark proc))))))))))
+(defun run-vpn-command (command)
+  (erase-buffer)
+  (make-process :name "vpn-command"
+                :buffer vpn-buffer
+                :command command
+                :coding '(utf-8-unix . utf-8-unix)
+                :filter (lambda (proc string)
+                          (when (buffer-live-p (process-buffer proc))
+                            (with-current-buffer (process-buffer proc)
+                              (let ((moving (= (point) (process-mark proc))))
+                                (save-excursion
+                                  (goto-char (process-mark proc))
+                                  (insert (replace-regexp-in-string "" "" string))
+                                  (set-marker (process-mark proc) (point)))
+                                (if moving (goto-char (process-mark proc)))))))))
 
 
-(define-vpn-command connect
-                    (list "bash" "-c"
-                          (format "printf '%s\n' | '%s' -s connect %s && mstsc connect.rdp & sleep 3s ; cat connected.txt; echo 'Press [D] to disconnect, [R] to reconnect'"
-                                  (vpn-helper-credentials-string vpn-password)
-                                  vpn-cli-exe
-                                  vpn-server)))
+(defun vpn-connect ()
+  (interactive)
+  (if connected
+      (run-vpn-command
+       (list "bash" "-c"
+             "mstsc connect.rdp & cat connected.txt ; echo 'Press [D] to disconnect, [R] to reconnect'"))
+    (run-vpn-command
+     (list "bash" "-c"
+           (format "printf '%s\n' | '%s' -s connect %s && mstsc connect.rdp & sleep 3s ; cat connected.txt; echo 'Press [D] to disconnect, [R] to reconnect'"
+                   (vpn-helper-credentials-string vpn-password)
+                   vpn-cli-exe
+                   vpn-server))))
+  (setq-local connected t))
 
 
-(define-vpn-command disconnect
-                    (list "bash" "-c"
-                          (format "'%s' disconnect ; cat disconnected.txt; echo 'Press [C] to connect'" vpn-cli-exe)))
+(defun vpn-disconnect ()
+  (interactive)
+  (run-vpn-command
+   (list "bash" "-c"
+         (format "'%s' disconnect ; cat disconnected.txt; echo 'Press [C] to connect'" vpn-cli-exe)))
+  (setq-local connected nil))
 
 
-(define-vpn-command reconnect
-                    (list "bash" "-c"
-                          (format "'%s' disconnect ; printf '%s\n' | '%s' -s connect %s && mstsc connect.rdp & sleep 3s ; cat connected.txt; echo 'Press [D] to disconnect, [R] to reconnect'"
-                                  vpn-cli-exe
-                                  (vpn-helper-credentials-string vpn-password)
-                                  vpn-cli-exe
-                                  vpn-server)))
+(defun vpn-reconnect ()
+  (interactive)
+  (run-vpn-command
+   (list "bash" "-c"
+         (format "'%s' disconnect ; printf '%s\n' | '%s' -s connect %s && mstsc connect.rdp & sleep 3s ; cat connected.txt; echo 'Press [D] to disconnect, [R] to reconnect'"
+                 vpn-cli-exe
+                 (vpn-helper-credentials-string vpn-password)
+                 vpn-cli-exe
+                 vpn-server))))
 
 
 (define-key global-map (kbd "C-c t") 'run-vpn)
